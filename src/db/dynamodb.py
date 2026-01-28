02215944ab8b1a95a216dae0c29273ad2d80ea1f
HassanAQ7 <hassan.allyqureshi@gmail.com>
2026-01-28 14:38:37 -0800

    added logging to dynamodb service and removed unused imports

--- src/db/dynamodb.py
+++ src/db/dynamodb.py
@@ -1,12 +1,10 @@
-import uuid
+import logging
+
 import boto3
 from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
-
 from src.config import settings
-from typing import List
-from datetime import datetime
-from src.models.models import EmotionResult, GameRecommendation, RecommendationResponse
+from src.models.models import RecommendationResponse
 
 
 class DynamoDBService:
@@ -19,6 +17,7 @@ class DynamoDBService:
 
     def put_recommendation(self, session_id: str, recommendation: RecommendationResponse):
         try:
+            logging.info("Attempting to put recommendation item in dynamodb")
             recommendation_item = {
                 'session_id': recommendation.session_id,
                 'recommendation_id': recommendation.recommendation_id,
@@ -27,19 +26,23 @@ class DynamoDBService:
                 'timestamp': int(recommendation.timestamp.timestamp() * 1000)
             }
             self.table.put_item(Item=recommendation_item)
+            logging.info("Successfully put recommendation in dynamodb table", recommendation_item)
         except ClientError as e:
-            print(e)
+            logging.error("Could not put recommendation in dynamodb table",
+                          e.response['Error']['Message'])
 
     def get_recent_recommendations(self, session_id: str, limit: int = 10):
         try:
+            logging.info("Attempting to get recent recommendations from dynamodb")
             recent_recommendations = self.table.query(
                 KeyConditionExpression=Key('session_id').eq(session_id),
                 Limit=limit,
                 ScanIndexForward=False
             )
         except ClientError as e:
-            print(e)
+            logging.error("Could not get recent recommendations", e.response['Error']['Message'])
         else:
+            logging.info("Successfully retrieved recommendations from dynamodb", recent_recommendations['Items'])
             return recent_recommendations['Items']
 
 
