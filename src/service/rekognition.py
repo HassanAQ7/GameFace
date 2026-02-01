bd077b66b80b69ddde4ab2b9376f77aea18ee413
HassanAQ7 <hassan.allyqureshi@gmail.com>
2026-01-31 23:45:53 -0800

    implemented proper error handling in rekognition service

--- src/service/rekognition.py
+++ src/service/rekognition.py
@@ -1,5 +1,7 @@
 import logging
 
+from botocore.exceptions import ClientError
+
 from src.config import settings
 import boto3
 
@@ -8,24 +10,39 @@ from src.models.models import EmotionResult
 
 class RekognitionService:
     def get_user_emotions(self, image: bytes) -> EmotionResult:
-        session = boto3.Session(
-            aws_access_key_id=settings.AWS_ACCESS_KEY,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION,
-            profile_name=settings.AWS_PROFILE_NAME
-        )
+        try:
+            session = boto3.Session(
+                aws_access_key_id=settings.AWS_ACCESS_KEY,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                region_name=settings.AWS_REGION,
+                profile_name=settings.AWS_PROFILE_NAME
+            )
 
-        client = session.client('rekognition')
-        logging.info("Getting user emotions from Rekognition")
-        response = client.detect_faces(Image={"Bytes": image}, Attributes=["ALL"])
-        detected_emotion = response['FaceDetails']['Emotions'][0]
-        logging.info(f"Rekognition detected {detected_emotion['Type']} with confidence {detected_emotion['Confidence']}")
-        emotion_result = EmotionResult(
-            confidence=detected_emotion['Confidence'],
-            emotion=detected_emotion['Type']
-        )
+            client = session.client('rekognition')
+            logging.info("Getting user emotions from Rekognition")
+            response = client.detect_faces(Image={"Bytes": image}, Attributes=["ALL"])
+            if not response['FaceDetails']:
+                logging.info("No faces detected in image, upload an image with clear view of face")
+                raise ValueError("Could not detect any faces detected in image")
+            if not response['FaceDetails'][0]['Emotions']:
+                logging.info("No emotions detected in image")
+                raise ValueError("Could not detect any emotions in image")
 
-        return emotion_result
+            detected_emotion = response['FaceDetails'][0]['Emotions'][0]
+            logging.info(f"Rekognition detected {detected_emotion['Type']} with confidence {detected_emotion['Confidence']}")
+            emotion_result = EmotionResult(
+                confidence=detected_emotion['Confidence'],
+                emotion=detected_emotion['Type']
+            )
+
+            return emotion_result
+        
+        except ClientError as e:
+            logging.error(f"Could not get user emotions from Rekognition: {e.response['Error']['Message']}")
+            raise
+        except Exception as e:
+            logging.error(f"Unknown error: {e}")
+            raise
 
 
 rekognition_service = RekognitionService()
