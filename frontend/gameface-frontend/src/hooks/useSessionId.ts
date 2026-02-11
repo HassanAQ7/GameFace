1a7f47c2aff80b17d3da6740c918515b3600fe9d
HassanAQ7 <hassan.allyqureshi@gmail.com>
2026-02-10 23:40:15 -0800

    created hook for retrieving session id

---
+++ frontend/gameface-frontend/src/hooks/useSessionId.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export function useSessionId() {
+    const[sessionId, setSessionId] = useState<string | null>(null)
+    useEffect(() => {
+        let id = localStorage.getItem('session-id')
+        if (!id) {
+            id = crypto.randomUUID()
+            localStorage.setItem('session-id', id)
+        }
+
+        setSessionId(sessionId)
+    }, []);
+
+    return sessionId;
+}
