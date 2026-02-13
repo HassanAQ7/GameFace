db2b8a2bbc7971362b3e6ce0b3a74f9be412a00c
HassanAQ7 <hassan.allyqureshi@gmail.com>
2026-02-12 23:58:00 -0800

    configured cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes.recommendations import recommendations_router
+
 app = FastAPI(
     name="GameFace",
     version="v1",
     description="backend for GameFace project"
 )
-app.include_router(recommendations_router, prefix=f"/api/v1/recommendations", tags=["recommendations"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(recommendations_router, prefix="/api/v1/recommendations", tags=["recommendations"])
