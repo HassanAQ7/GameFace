b442e3e6e84729cc126b77c844e2e85901ba35eb
HassanAQ7 <hassan.allyqureshi@gmail.com>
2026-02-13 21:48:34 -0800

    fixed issue with variable needs VITE prefix

--- frontend/gameface-frontend/src/util/api.ts
+++ frontend/gameface-frontend/src/util/api.ts
@@ -1,6 +1,6 @@
 import { type EmotionResult, type GameRecommendation, type RecommendationResponse } from "./types";
 
-const API_BASE_URL = import.meta.env.API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const gameRecommendationAPI = {
   getRecommendations: async (
